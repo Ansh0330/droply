a303109e3854a982509bb07a6795502978ca0e86
Ansh Kotnala <meanshbhardwaj@gmail.com>
2025-08-16 15:25:56 +0530

    added signin form

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export const page = () => {
+export default function page() {
   return (
-    <div>App page</div>
+    <div>page</div>
   )
 }
--- schemas/signinSchema.ts
+++ schemas/signinSchema.ts
@@ -1,6 +1,6 @@
 import * as z from "zod";
 
-export const signinSchema = z.object({
+export const signInSchema = z.object({
   identifier: z
     .string()
     .min(1, { message: "Email is required" })
