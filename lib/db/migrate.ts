7cfb8d293f221acfee4fbc5e30898ea50a310e10
Ansh Kotnala <meanshbhardwaj@gmail.com>
2025-08-16 13:16:26 +0530

    first commit

---
+++ lib/db/migrate.ts
@@ -0,0 +1,36 @@
+import { migrate } from "drizzle-orm/neon-http/migrator";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import * as dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env",
+});
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("Database Url is not set in the environment variables");
+}
+
+async function runMigration() {
+  console.log("🔄 Starting database migration...");
+
+  try {
+    // Create a Neon SQL connection
+    const sql = neon(process.env.DATABASE_URL!);
+
+    // Initialize Drizzle with the connection
+    const db = drizzle(sql);
+
+    // Run migrations from the drizzle folder
+    console.log("📂 Running migrations from ./drizzle folder");
+    await migrate(db, { migrationsFolder: "./drizzle" });
+
+    console.log("✅ Database migration completed successfully!");
+  } catch (error) {
+    console.error("❌ Migration failed:", error);
+    process.exit(1);
+  }
+}
+
+// Run the migration
+runMigration();
---
+++ lib/db/schema.ts
@@ -0,0 +1,54 @@
+import {
+  pgTable,
+  text,
+  uuid,
+  integer,
+  boolean,
+  timestamp,
+} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+
+export const files = pgTable("files", {
+  id: uuid("id").defaultRandom().primaryKey(),
+
+  //basic file/folder information
+  name: text("name").notNull(),
+  path: text("path").notNull(), // document/project/resume
+  size: integer("size").notNull(),
+  type: text("type").notNull(), // folder
+
+  // storage information
+  fileUrl: text("file_url").notNull(), // url to access file
+  thumbnailUrl: text("thumbnail_url"), // url to access thumbnail
+
+  // Ownership information
+  userId: text("user_id").notNull(),
+  parentId: uuid("parent_id"), // parent folder id (null for root items)
+
+  // files / folder flags
+  isFolder: boolean("is_folder").default(false).notNull(),
+  isStarred: boolean("is_starred").default(false).notNull(),
+  isTrash: boolean("is_trash").default(false).notNull(),
+
+  // timestamps
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+/*
+parent : Each file/folder can have one parent folder
+children : Each folder can have many child files / folder
+*/
+
+export const filesRelation = relations(files, ({ one, many }) => ({
+  parent: one(files, {
+    fields: [files.parentId],
+    references: [files.id],
+  }),
+  // relationship to child files/folder
+  children: many(files),
+}));
+
+// Type definitions
+export const File = typeof files.$inferSelect
+export const NewFile = typeof files.$inferInsert
